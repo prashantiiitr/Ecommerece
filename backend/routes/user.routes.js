67498a4eb364dcd6aeeb621efa5825c1fdab7a17
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2025-02-14 19:31:06 +0530

    added login

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -13,5 +13,15 @@ router.post('/register', [
 ],
   userController.registerUser
 )
+router.post('/login', [
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+  userController.loginUser
+)
 
-module.exports=router
+
+
+
+
+module.exports = router;
