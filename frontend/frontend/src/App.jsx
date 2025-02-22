0d71f4043d5f7463b02fc0403fd3b7dec88f30fe
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2025-02-22 15:08:25 +0530

    Addeddifferent Pages

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import react from 'react';
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import UserLogin from './pages/UserLogin';
+import UserSignup from './pages/UserSignup';
+import CaptainLogin from './pages/CaptainLogin';
+import CaptainSignup from './pages/CaptainSignup';
+
+  const App=()=>{
+
+  return (
+    <>
+     <Routes>
+      <Route path="/Home" element={<Home />} />
+      <Route path="/login" element={<UserLogin />} />
+      <Route path="/signup" element={<UserSignup />} />
+      <Route path="/captain-login" element={<CaptainLogin />} />
+      <Route path="/captain-signup" element={<CaptainSignup />} />
+
+     </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+ const Home=()=>{
+  return (
+    <>
+     <div className='h-screen w-full bg-red-400'>
+      <div></div>
+     </div>
+    </>
+  )
+ }
+ export default Home
